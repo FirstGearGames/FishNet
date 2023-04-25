52d60b639d62272cc1dce531eb6aaae3a4d3dc50
Tinh Le <lkai355@yahoo.com>
2023-04-25 21:35:32 +0700

    Add networkobject QOL extent

---
+++ Assets/FishNet/Runtime/Object/NetworkObject.QOLExtent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace FishNet.Object
+{
+    public sealed partial class NetworkObject : MonoBehaviour
+    {
+        public bool TryGetNetworkBehaviour(byte componentIndex, out NetworkBehaviour behaviour)
+        {
+            behaviour = null;
+            if (componentIndex >= NetworkBehaviours.Length)
+                return false;
+            behaviour = NetworkBehaviours[componentIndex];
+            return true;
+        }
+        public bool TryGetNetworkBehaviour<T>(out T behaviour) where T : NetworkBehaviour
+        {
+            behaviour = null;
+            foreach (var b in NetworkBehaviours)
+            {
+                if (b is T target)
+                {
+                    behaviour = target;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+
