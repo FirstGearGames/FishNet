dc44a6df5ba09138ef3c65a6311679a7b1cfd44b
FirstGearGames <firstgeargames@gmail.com>
2024-08-26 11:18:41 -0400

    4.4.4 - Include Edgegap
    - Fixed SyncTypes corrupting spawn packets.

--- Assets/FishNet/Upgrading/EdgegapMenu.cs
+++ Assets/FishNet/Upgrading/EdgegapMenu.cs
@@ -1,26 +1,26 @@
-﻿#if !EDGEGAP_PLUGIN_SERVERS
-#if UNITY_EDITOR
-using FishNet.Documenting;
-using UnityEditor;
-using UnityEngine;
-
-namespace FishNet.Plugin
-{
-    [APIExclude]
-    public class EdgegapMenu : MonoBehaviour
-    {
-
-        /// <summary>
-        /// Replaces all components.
-        /// </summary>
-        [MenuItem("Tools/Get Edgegap Hosting", false, 0)]
-        private static void GetEdgegapHosting()
-        {
-            Application.OpenURL("https://firstgeargames.com/FishNet/Edgegap/");
-        }
-
-
-    }
-}
-#endif
-#endif
+﻿// #if !EDGEGAP_PLUGIN_SERVERS
+// #if UNITY_EDITOR
+// using FishNet.Documenting;
+// using UnityEditor;
+// using UnityEngine;
+//
+// namespace FishNet.Plugin
+// {
+//     [APIExclude]
+//     public class EdgegapMenu : MonoBehaviour
+//     {
+//
+//         /// <summary>
+//         /// Replaces all components.
+//         /// </summary>
+//         [MenuItem("Tools/Get Edgegap Hosting", false, 0)]
+//         private static void GetEdgegapHosting()
+//         {
+//             Application.OpenURL("https://firstgeargames.com/FishNet/Edgegap/");
+//         }
+//
+//
+//     }
+// }
+// #endif
+// #endif
