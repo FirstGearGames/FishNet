8b53121a44694d460a6e87b86227678f00cb8238
Array <88677523+youngwoocho02@users.noreply.github.com>
2025-06-22 05:15:23 +0900

    Revert "change to use static not kinematic at rigidbody 2d"
    This reverts commit c4007ea8795e6b659a25df5d501e9b9290486ff4.

--- Assets/FishNet/Runtime/Generated/Component/NetworkTransform/NetworkTransform.cs
+++ Assets/FishNet/Runtime/Generated/Component/NetworkTransform/NetworkTransform.cs
@@ -825,7 +825,8 @@ namespace FishNet.Component.Transforming
                         _initializedRigidbodyInterpolation2d = c.interpolation;
 
                     bool isKinematic = CanMakeKinematic();
-                    c.bodyType = isKinematic ? RigidbodyType2D.Static : RigidbodyType2D.Dynamic;
+                    c.isKinematic = isKinematic;
+                    c.simulated = !isKinematic;
 
                     if (isKinematic)
                         c.interpolation = RigidbodyInterpolation2D.None;
@@ -1619,16 +1620,16 @@ namespace FishNet.Component.Transforming
                 //No more in buffer, see if can extrapolate.
                 else
                 {
-
-                    /* If everything matches up then end queue.
-                     * Otherwise let it play out until stuff
-                     * aligns. Generally the time remaining is enough
-                     * but every once in awhile something goes funky
-                     * and it's thrown off. */
-                    if (!HasChanged(td))
-                        _currentGoalData = null;
-                    OnInterpolationComplete?.Invoke();
-
+                    
+                        /* If everything matches up then end queue.
+                         * Otherwise let it play out until stuff
+                         * aligns. Generally the time remaining is enough
+                         * but every once in awhile something goes funky
+                         * and it's thrown off. */
+                        if (!HasChanged(td))
+                            _currentGoalData = null;
+                        OnInterpolationComplete?.Invoke();
+                        
                 }
             }
         }
@@ -2134,7 +2135,7 @@ namespace FishNet.Component.Transforming
             //Default value.
             next.ExtrapolationState = TransformData.ExtrapolateState.Disabled;
 
-
+            
         }
 
         /// <summary>
